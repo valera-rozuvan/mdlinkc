1c960ec04539944b5811dbc537afefd266e03ad9
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-01-25 13:12:26 +0200

    Creating bin files.

---
+++ bin/main.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getLinks() {
+    return [
+        'link1',
+        'link2'
+    ];
+}
+exports.Mdlinkc = {
+    getLinks
+};
+//# sourceMappingURL=main.js.map
